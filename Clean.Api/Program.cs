26ae7e28915290e132d1f693756cdde751e0430e
pouyan alizadeh <pouyan9913@gmail.com>
2024-09-26 18:29:39 +0330

    init

--- Clean.Api/Program.cs
+++ Clean.Api/Program.cs
@@ -13,11 +13,9 @@ builder.Services
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
